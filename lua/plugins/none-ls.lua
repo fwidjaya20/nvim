50c3bbf6ecc805bc13535615db59ee0e5eb3dd62
Fredrick Widjaya <f.widjaya20@gmail.com>
2025-06-15 12:37:44 +0700

    feat: using gofmt and wsl for golang

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,43 +1,44 @@
 return {
-  {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-      "nvimtools/none-ls-extras.nvim",
-    },
-    config = function()
-      local nls = require("null-ls")
-      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = {
+			"nvimtools/none-ls-extras.nvim",
+		},
+		config = function()
+			local nls = require("null-ls")
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-      nls.setup({
-        sources = {
-          nls.builtins.code_actions.gomodifytags,
-          nls.builtins.code_actions.impl,
-          nls.builtins.completion.spell,
-          nls.builtins.formatting.gofumpt,
-          nls.builtins.formatting.goimports,
-          nls.builtins.formatting.prettier,
-          nls.builtins.formatting.stylua,
-          nls.builtins.formatting.golines,
-          require("none-ls.diagnostics.eslint_d"),
-        },
-        on_attach = function(client, bufnr)
-          if client:supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format({ async = false, bufnr = bufnr })
-              end,
-            })
-          end
-        end,
-      })
+			nls.setup({
+				sources = {
+					nls.builtins.code_actions.gomodifytags,
+					nls.builtins.code_actions.impl,
+					nls.builtins.completion.spell,
+					nls.builtins.formatting.gofmt,
+					nls.builtins.formatting.goimports,
+					nls.builtins.formatting.golines,
+					nls.builtins.formatting.prettier,
+					nls.builtins.formatting.stylua,
+					nls.builtins.formatting.wsl,
+					require("none-ls.diagnostics.eslint_d"),
+				},
+				on_attach = function(client, bufnr)
+					if client:supports_method("textDocument/formatting") then
+						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							group = augroup,
+							buffer = bufnr,
+							callback = function()
+								vim.lsp.buf.format({ async = false, bufnr = bufnr })
+							end,
+						})
+					end
+				end,
+			})
 
-      vim.g.lazyvim_eslint_auto_format = true
-      vim.g.lazyvim_prettier_needs_config = true
+			vim.g.lazyvim_eslint_auto_format = true
+			vim.g.lazyvim_prettier_needs_config = true
 
-      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
-  },
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		end,
+	},
 }
