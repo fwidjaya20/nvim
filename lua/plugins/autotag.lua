b56869ac693f2a837531832f5924ba1d1b4027ca
Fredrick Widjaya <f.widjaya20@gmail.com>
2025-06-15 10:40:14 +0700

    feat: add string surround

--- lua/plugins/autotag.lua
+++ lua/plugins/autotag.lua
@@ -1,15 +1,20 @@
 return {
-  {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true,
-  },
-  {
-    'windwp/nvim-ts-autotag',
-    opts = {
-      enable_close = true,
-      enable_rename = true,
-      enable_close_on_slash = false,
-    }
-  },
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		opts = {
+			enable_close = true,
+			enable_rename = true,
+			enable_close_on_slash = false,
+		},
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+	},
 }
