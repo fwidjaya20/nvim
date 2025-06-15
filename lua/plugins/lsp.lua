7a19c5acf70956d891c17f0fc3b51319f42e9875
Fredrick Widjaya <f.widjaya20@gmail.com>
2025-06-15 12:32:15 +0700

    feat: add golang formatter golines

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,118 +1,93 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "eslint_d",
-        "gopls",
-        "lua_ls",
-        "prettier",
-        "stylua",
-        "tailwindcss",
-        "ts_ls",
-        "golangci_lint",
-      },
-    },
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-        "tailwindcss",
-        "ts_ls",
-        "gopls",
-      },
-    },
-    dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
-      "neovim/nvim-lspconfig",
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "onsails/lspkind.nvim",
-    },
-    opts = {
-      servers = {
-        gopls = {
-          settings = {
-            gopls = {
-              gofumpt = true,
-              codelenses = {
-                gc_details = false,
-                generate = true,
-                regenerate_cgo = true,
-                run_govulncheck = true,
-                test = true,
-                tidy = true,
-                upgrade_dependency = true,
-                vendor = true,
-              },
-              hints = {
-                assignVariableTypes = true,
-                compositeLiteralFields = true,
-                compositeLiteralTypes = true,
-                constantValues = true,
-                functionTypeParameters = true,
-                parameterNames = true,
-                rangeVariableTypes = true,
-              },
-              analyses = {
-                nilness = true,
-                unusedparams = true,
-                unusedwrite = true,
-                useany = true,
-              },
-              usePlaceholders = true,
-              completeUnimported = true,
-              staticcheck = true,
-              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
-              semanticTokens = true,
-            },
-          },
-        },
-        tailwindcss = {},
-      },
-    },
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"mason-org/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"eslint_d",
+				"gopls",
+				"lua_ls",
+				"prettier",
+				"stylua",
+				"tailwindcss",
+				"ts_ls",
+				"golangci_lint",
+			},
+		},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"tailwindcss",
+				"ts_ls",
+				"gopls",
+			},
+		},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"onsails/lspkind.nvim",
+		},
+		opts = {
+			servers = {
+				gopls = {
+					settings = {
+						gopls = {
+							analyses = {
+								unusedparams = true,
+								useany = true,
+							},
+							usePlaceholders = true,
+							completeUnimported = true,
+						},
+					},
+				},
+				tailwindcss = {},
+			},
+		},
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      vim.lsp.enable("lua_ls")
-      vim.lsp.config("lua_ls", {
-        capabilities = capabilities,
-      })
+			vim.lsp.enable("lua_ls")
+			vim.lsp.config("lua_ls", {
+				capabilities = capabilities,
+			})
 
-      vim.lsp.enable("gopls")
-      vim.lsp.config("gopls", {
-        capabilities = capabilities,
-      })
+			vim.lsp.enable("gopls")
+			vim.lsp.config("gopls", {
+				capabilities = capabilities,
+			})
 
-      vim.lsp.enable("ts_ls")
-      vim.lsp.config("ts_ls", {
-        capabilities = capabilities,
-      })
+			vim.lsp.enable("ts_ls")
+			vim.lsp.config("ts_ls", {
+				capabilities = capabilities,
+			})
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-      vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
-      require("telescope").load_extension("ui-select")
-    end,
-  },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
