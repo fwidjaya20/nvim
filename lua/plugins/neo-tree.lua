6c1eca25cea23c472ee8c4eb27906ff3f0add2cb
Fredrick Widjaya <f.widjaya20@gmail.com>
2025-06-16 08:49:25 +0700

    feat: always show hidden items on neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,13 +1,26 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",  
-    "MunifTanjim/nui.nvim",
-  },
-  lazy = false,
-  config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>' )
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	lazy = false,
+	opts = {
+		filesystem = {
+			filtered_items = {
+				hide_by_name = {
+					".git",
+				},
+				hide_dotfiles = false,
+				hide_gitignored = false,
+				show_hidden_count = true,
+				visible = true,
+			},
+		},
+	},
+	keys = {
+		{ "<C-n>", "<cmd>Neotree filesystem reveal left<CR>", desc = "Toggle Neo-tree" },
+	},
 }
